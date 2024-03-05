aa4738ce0cadea407f8d6889641946a38c37f11b
GabPSS <45743069+GabPSS@users.noreply.github.com>
2024-03-05 12:45:33 -0300

    Added keys file

---
+++ lib/api/keys.dart
@@ -0,0 +1,9 @@
+///
+/// This file contains project URLs and keys for a Supabase project hosted on
+/// the Supabase platform. Replace them with your own keys and URL if you wish
+/// to host it yourself
+///
+
+//TODO: Replace API keys
+const String apiUrl = "";
+const String apiKey = "";
